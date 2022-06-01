687f8bda92da4d29c96d3066fc7ff46e3d0c506f
jasmine-jp <61972863+jasmine-jp@users.noreply.github.com>
2022-06-01 18:53:15 +0900

    enable to move at the same time

--- main.py
+++ main.py
@@ -4,16 +4,20 @@ from player import Player
 window_x, window_y = 160, 120
 pyxel.init(window_x, window_y)
 
-player = Player(
+player1 = Player(
     (window_x, window_y),
     [0, 0],
-    [window_x-20, 0],
     [
         pyxel.KEY_E,
         pyxel.KEY_X,
         pyxel.KEY_S,
         pyxel.KEY_F
-    ],
+    ]
+)
+
+player2 = Player(
+    (window_x, window_y),
+    [window_x-20, 0],
     [
         pyxel.KEY_I,
         pyxel.KEY_M,
@@ -22,4 +26,13 @@ player = Player(
     ]
 )
 
-pyxel.run(player.update, player.draw)
+def update():
+    player1.update()
+    player2.update()
+
+def draw():
+    pyxel.cls(0)
+    player1.draw()
+    player2.draw()
+
+pyxel.run(update, draw)
